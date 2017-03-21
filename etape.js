5c890bad96c0d5644832e2f1d96ac50d439a015e
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-03-21 15:19:42 -0400

    Étape 1 - Test numéro 2

--- etape.js
+++ etape.js
@@ -9,46 +9,28 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
-/******REQUIRE*********/
-var http = require('http');
-var url = require('url');
-
-
 /*******VARIABLES******/
 
 /**************FONCTIONS**********/
 
 // Créer un serveur
-http.createServer( function (request, response) {  
-   // On extrait de la requête «request» le chemin  qui nous donnera le nom de fichier
-   var pathname = url.parse(request.url).pathname;
-   
- 
-   console.log("pathname= " + pathname)
-  
-  // affiche le nom du fichier pour laquelle la requête a été généré
-   
+app.get('/', function (req, res) {
+    
+    
+fs.readFile('collection-provinces.json', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
 
-   // Lire par le «fs» (file système) le fichier de la requête 
-   fs.readFile(pathname.slice(1), function (err, data) {
-      if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html ; charset=utf-8'});
-      }else { 
-         //Page found   
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});  
-         
-         // affiche le contenu du fichier dans la page HTML
-         response.write(data.toString());   
-      }
-      // transmet la reponse  
-      response.write(data.toString());
-   });   
-}).listen(8082);
+console.log("Program Ended");
+})
+
+var server = app.listen(8082, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
 
 // message console
 console.log('Serveur se trouvant à http://127.0.0.1:8082/');
