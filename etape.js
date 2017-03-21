25bc00a8ce844da6b3341d7e794d0d8165d48e22
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-03-21 16:04:21 -0400

    Étape 2 - Afficher index.ejs

--- etape.js
+++ etape.js
@@ -57,47 +57,20 @@ console.log('Serveur se trouvant à http://127.0.0.1:8082/');
 
 /***********ÉTAPE 2***************/
 
-http.createServer( function (request, response) {  
-   // On extrait de la requête «request» le chemin  qui nous donnera le nom de fichier
-   var pathname = url.parse(request.url).pathname;
+app.get('/', function (req, res) {
+   console.log('la route route get / = ' + req.url)
+    fs.readFile('views/index.ejs', function (err, data) {
+   if (err) return console.error(err);
+   res.write(data.toString());
+    });
+
+    console.log("Program Ended");
+    
+})
+
+var server = app.listen(8082, function () {
+   var host = server.address().address
+   var port = server.address().port
    
- 
-   console.log("pathname= " + pathname)
-  
-  // affiche le nom du fichier pour laquelle la requête a été généré
-   
-
-   // Lire par le «fs» (file système) le fichier de la requête 
-   fs.readFile("views/index.ejs", function (err, data) {
-      var fName = "collection_provinces.json";
-       if (err) {
-         console.log(err);
-         // HTTP Status: 404 : NOT FOUND
-         // Content Type: text/plain
-         response.writeHead(404, {'Content-Type': 'text/html ; charset=utf-8'});
-      }else { 
-         //Page found   
-         // HTTP Status: 200 : OK
-         // Content Type: text/plain
-         response.writeHead(200, {'Content-Type': 'text/html'});  
-
-          function genererTab(obj){
-            var sChaine = "";
-            for (fName in obj){
-               sChaine += "<tr><td>" + fName + "</td><td>" + obj[fName].toString() + "</td></tr>";
-            }
-            return sChaine;
-         }
-         // affiche le contenu du fichier dans la page HTML
-         response.render('index.ejs');
-          
-          
-         console.log(data.toString());
-      }
-      // transmet la reponse  
-      response.end();
-   });   
-}).listen(8082);
-
-// message console
-console.log('Serveur se trouvant à http://127.0.0.1:8082/');
+   console.log("Example app listening at http://%s:%s", host, port)
+})
