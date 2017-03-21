f29ba992026db85300fbd5923c6fba1828cc5720
Patrick Lauzon <patrick.lauzon94@hotmail.com>
2017-03-21 15:35:10 -0400

    Étape 1 - Ajustement -> l'erreur était collection-provinces.json - > collection_provinces.json //  Étape 1 Fini //

--- etape.js
+++ etape.js
@@ -9,28 +9,47 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
+/******REQUIRE*********/
+var http = require('http');
+var url = require('url');
+
+
 /*******VARIABLES******/
 
 /**************FONCTIONS**********/
 
 // Créer un serveur
-app.get('/', function (req, res) {
-    
-    
-fs.readFile('collection-provinces.json', function (err, data) {
-   if (err) return console.error(err);
-   console.log(data.toString());
-});
-
-console.log("Program Ended");
-})
-
-var server = app.listen(8082, function () {
-   var host = server.address().address
-   var port = server.address().port
+http.createServer( function (request, response) {  
+   // On extrait de la requête «request» le chemin  qui nous donnera le nom de fichier
+   var pathname = url.parse(request.url).pathname;
    
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+ 
+   console.log("pathname= " + pathname)
+  
+  // affiche le nom du fichier pour laquelle la requête a été généré
+   
+
+   // Lire par le «fs» (file système) le fichier de la requête 
+   fs.readFile("public/text/collection_provinces.json", function (err, data) {
+      if (err) {
+         console.log(err);
+         // HTTP Status: 404 : NOT FOUND
+         // Content Type: text/plain
+         response.writeHead(404, {'Content-Type': 'text/html ; charset=utf-8'});
+      }else { 
+         //Page found   
+         // HTTP Status: 200 : OK
+         // Content Type: text/plain
+         response.writeHead(200, {'Content-Type': 'text/html'});  
+         
+         // affiche le contenu du fichier dans la page HTML
+         response.write(data.toString());
+         console.log(data.toString());
+      }
+      // transmet la reponse  
+      response.end();
+   });   
+}).listen(8082);
 
 // message console
 console.log('Serveur se trouvant à http://127.0.0.1:8082/');
